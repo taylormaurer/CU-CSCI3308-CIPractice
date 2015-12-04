df7cf2fe536a22afbac0461b4f60a8bb24adac57
Taylor Maurer <taylor.maurer@colorado.edu>
2015-12-04 11:23:32 -0700

    Thing

--- geometry_test.c
+++ geometry_test.c
@@ -50,7 +50,6 @@ START_TEST(test_2d_eq)
     b.x = 7.77;
     b.y = 8.88;
     ck_assert(!coord_2d_eq(&a, &b));
-
 }
 END_TEST
 
@@ -145,6 +144,31 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+
+START_TEST(test_2d_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    a.x = 5.0;
+    a.y = 5.0;
+    b.x = 30.0;
+    b.y = 15.0;
+    c.x = 50.0;
+    c.y = 5.0;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 225);
+}
+END_TEST
+
+
+
+
+
+
+
+
+
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,11 +186,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area);
     /* Return Suite */
     return s;
 
@@ -183,5 +210,6 @@ int main(void){
     srunner_free(sr);
 
     return (failed ? EXIT_FAILURE : EXIT_SUCCESS);
+
 
 }
